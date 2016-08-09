187bc00df50ec424b6b01c6de1b9a5446c06286b
Donaldson, Mitchell <Mitchell.Donaldson@news.com.au>
2016-08-09 17:26:54 +1000

    First commit

---
+++ index.js
@@ -0,0 +1,64 @@
+/*global require, module*/
+var ApiBuilder = require('claudia-api-builder'),
+	AWS = require('aws-sdk'),
+	api = new ApiBuilder(),
+	dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+module.exports = api;
+
+// Create new user
+api.post('/user', function (request) {
+	'use strict';
+	var params = {
+		TableName: request.env.tableName,
+		Item: {
+			userid: request.body.userid,
+			name: request.body.name,
+			age: request.body.age
+		}
+	};
+	// return dynamo result directly
+	return dynamoDb.put(params).promise();
+}, { success: 201 }); // Return HTTP status 201 - Created when successful
+
+// get user for {id}
+api.get('/user/{id}', function (request) {
+	'use strict';
+	var id, params;
+	// Get the id from the pathParams
+	id = request.pathParams.id;
+	params = {
+		TableName: request.env.tableName,
+		Key: {
+			userid: id
+		}
+	};
+  console.log("Working here");
+	// post-process dynamo result before returning
+	return dynamoDb.get(params).promise().then(function (response) {
+		return response.Item;
+	});
+});
+
+
+
+// delete user with {id}
+api.delete('/user/{id}', function (request) {
+	'use strict';
+	var id, params;
+	// Get the id from the pathParams
+	id = request.pathParams.id;
+	params = {
+		TableName: request.env.tableName,
+		Key: {
+			userid: id
+		}
+	};
+	// return a completely different result when dynamo completes
+	return dynamoDb.delete(params).promise()
+		.then(function () {
+			return 'Deleted user with id "' + id + '"';
+		});
+}, {success: { contentType: 'text/plain'}});
+
+api.addPostDeployConfig('tableName', 'DynamoDB Table Name:', 'configure-db');
